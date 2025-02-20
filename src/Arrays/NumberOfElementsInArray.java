f2335efbd382360b770939bb077d93068f2d8c85
Poovarasan H <poova8069@gmail.com>
2025-02-20 15:30:27 +0530

    1st Program Created

--- src/Arrays/NumberOfElementsInArray.java
+++ src/Arrays/NumberOfElementsInArray.java
@@ -1,7 +1,21 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class NumberOfElementsInArray {
     public static void main(String[] args) {
-
+    /*
+   Input:
+        10 20 30 40 50
+    Output:
+        Number of elements in the array: 5
+     */
+        int a[] = {10, 20, 30, 40, 50};
+        int count=0;
+        for (int i = 0; i < a.length; i++) {
+            count=count+1;
+        }
+        System.out.println("Number of Elements in an Array:" +count);
     }
+
 }
